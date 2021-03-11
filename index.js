cf3932bd632080bce77f2dcb5304e49c04d1f7b8
Ryan Manchester <ryanm17@gmail.com>
2021-03-11 14:27:57 -0500

    creates initial api request function

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+
+//API key = pk_73008ed623f14d1691b1072e091055be
+
+//callApi function
+
+const callApi = (finishedApi) => {
+  request('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_73008ed623f14d1691b1072e091055be', {json: true}, (err, resp, body) => {
+    if (err) {
+      return err
+    };
+    if (resp.statusCode === 200) {
+      finishedApi(body);
+    };
+  });
+}
+
+//Test API request
+
 
 //Set listening port to deployed env or local host
 const PORT = process.env.PORT || 3000;
@@ -13,11 +32,16 @@ app.set('view engine', 'handlebars');
 
 //Set Handlebars routes
 app.get('/', function (req, res) {
+  callApi( function(doneApi) {
     res.render('home', {
-      stuff: "This is some stuff"
+      stock: doneApi
     });
-
+  });
 });
+
+app.get('/about.html', function(req, res) {
+  res.render('about')
+})
 
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
