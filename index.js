5999220b13df4f49a8ee746e09ce0ff1b90df019
Ryan Manchester <ryanm17@gmail.com>
2021-03-16 22:21:12 -0400

    connects search form to API

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser')
 const request = require('request');
 
 //API key = pk_73008ed623f14d1691b1072e091055be
 
 //callApi function
 
-const callApi = (finishedApi) => {
-  request('https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_73008ed623f14d1691b1072e091055be', {json: true}, (err, resp, body) => {
+const callApi = (finishedApi, ticker) => {
+  request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_73008ed623f14d1691b1072e091055be', {json: true}, (err, resp, body) => {
     if (err) {
       return err
     };
@@ -26,17 +27,30 @@ const callApi = (finishedApi) => {
 //Set listening port to deployed env or local host
 const PORT = process.env.PORT || 3000;
 
+//Set body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //Set Handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set Handlebars routes
+//Set Handlebars index GET route
 app.get('/', function (req, res) {
   callApi( function(doneApi) {
     res.render('home', {
       stock: doneApi
     });
   });
+});
+
+//Set Handlebars POST route
+app.post('/', function (req, res) {
+  const ticker = req.body.stockTicker;
+  callApi( function(doneApi) {
+    res.render('home', {
+      stock: doneApi
+    });
+  },ticker );
 });
 
 app.get('/about.html', function(req, res) {
